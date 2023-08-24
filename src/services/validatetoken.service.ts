71e8d73d8e6b7186b22ce81d7a3e1e9ece5bb0bf
JohanRN <Johanrojasn21@gmail.com>
2023-08-23 23:14:32 -0500

    Update

--- src/services/validatetoken.service.ts
+++ src/services/validatetoken.service.ts
@@ -1,7 +1,7 @@
 export function isValidApiKey(event: any): void {
     const apiKey = event.headers.Authorization
     if (apiKey !== 'Bearer pk_test_LsRb12fdsvdew21') {
-        throw new Error('Clave de API no válida');
+        throw new Error('El token header es invalido');
     }
 }
 
